b836dcbeaed18154e674c382b0295420ee594328
Karine Débora Wagner <karinedwagner@gmail.com>
2026-01-19 16:17:05 -0400

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-slate-900 flex h-screen items-center justify-center">
+      <h1 className="text-5xl text-white">Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
