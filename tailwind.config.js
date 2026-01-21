ec208d889c6690ecabeacc677f679adab37b013c
Karine Débora Wagner <karinedwagner@gmail.com>
2026-01-21 17:07:23 -0400

    Configura o taiwlind para a versão 3 e cria algumas propriedade padrão

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#eca413",
+        "backgroundLight": "#fcfaf8",
+        "backgroundDark": "#221c10",
+      },
+      fontFamily: {
+        "display": ["Plus Jakarta Sans"]
+      },
+      borderRadius: { "DEFAULT": "0.5rem", "lg": "1rem", "xl": "1.5rem", "full": "9999px" },
+    },
   },
   plugins: [],
 }
