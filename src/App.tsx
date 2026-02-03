0ef4a72bad750379d6d2a41987b0bc1eb2d76056
Karine Débora Wagner <karinedwagner@gmail.com>
2026-02-02 21:03:37 -0400

    Addiciona a rota de dashboard de tutores "/tutors"

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import { ProtectedRoute } from './components/ProtectedRoute';
 
 import { LoginPage } from './pages/Login';
 import { DashboardPetsPage } from './pages/DashboardPetsPage';
+import { DashboardTutorsPage } from './pages/DashboardTutorsPage';
 import { PetDetailPage } from './pages/PetDetails';
 import { PetFormPage } from './pages/PetForm';
-
 
 function App() {
   return (
@@ -24,6 +24,7 @@ function App() {
           <Route path="/pets/:petId" element={<PetDetailPage />} />
           <Route path="/pets/edit/:id" element={<PetFormPage />} />
           <Route path="/pets/new" element={<PetFormPage />} />
+          <Route path="/tutors" element={<DashboardTutorsPage />} />
         </Route>
       </Route>
     </Routes>
