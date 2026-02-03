a117640aa81d11de11c5b0e04b91190a8fe0e676
Karine Débora Wagner <karinedwagner@gmail.com>
2026-02-03 13:29:36 -0400

    Feat: Atualiza o component FormInput, incluir validação do Zod

--- src/components/FormInput/index.tsx
+++ src/components/FormInput/index.tsx
@@ -1,28 +1,39 @@
-interface InputProps {
+interface FormInputProps {
     label: string;
     value: string | number;
     type?: string;
     disabled?: boolean;
-    onChange: (value: string) => void;
+    error?: string;
+    onChange: (v: string) => void;
 }
 
 export function FormInput({
     label,
     value,
     type = 'text',
-    disabled = false,
+    disabled,
+    error,
     onChange,
-}: InputProps) {
+}: FormInputProps) {
     return (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-1">
             <label className="text-sm font-semibold">{label}</label>
+
             <input
                 type={type}
                 value={value}
                 disabled={disabled}
                 onChange={(e) => onChange(e.target.value)}
-                className="h-14 rounded-xl px-4 border bg-[#fcfaf8] dark:bg-[#221c10]"
+                className={`h-14 rounded-xl px-4 border 
+          ${error ? 'border-red-500' : 'border-gray-300'}
+        `}
             />
+
+            {error && (
+                <span className="text-sm text-red-500">
+                    {error}
+                </span>
+            )}
         </div>
     );
 }
