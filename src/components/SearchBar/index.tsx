abf592c189e268be407b5690732172f3f9095069
Karine Débora Wagner <karinedwagner@gmail.com>
2026-02-02 21:08:06 -0400

    Feat: Adiciona uma validação par mostrar filtro por raca em caso de ser pesquisa de pets

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,5 +1,6 @@
 interface SearchBarProps {
   nome: string,
+  tipo: string,
   raca: string,
   setNome: (nome: string) => void,
   setRaca: (raca: string) => void,
@@ -8,6 +9,7 @@ interface SearchBarProps {
 
 export function SearchBar({
   nome,
+  tipo,
   raca,
   setNome,
   setRaca,
@@ -35,18 +37,22 @@ export function SearchBar({
           onChange={(e) => setNome(e.target.value)}
         />
       </div>
-      <div className="flex-1 min-w-[240px] relative">
-        <span className="material-symbols-outlined absolute left-4 top-1/2 -translate-y-1/2 text-[#a1957e]">
-          pets
-        </span>
-        <input
-          className="form-input w-full h-12 pl-11 pr-4 rounded-lg bg-[#f8f7f6] dark:bg-[#221c10] border-none focus:ring-2 focus:ring-primary text-sm text-[#1b170d] dark:text-white"
-          placeholder="Buscar por Raça"
-          type="text"
-          value={raca}
-          onChange={(e) => setRaca(e.target.value)}
-        />
-      </div>
+      {
+        tipo === 'pet' && (
+          <div className="flex-1 min-w-[240px] relative">
+            <span className="material-symbols-outlined absolute left-4 top-1/2 -translate-y-1/2 text-[#a1957e]">
+              pets
+            </span>
+            <input
+              className="form-input w-full h-12 pl-11 pr-4 rounded-lg bg-[#f8f7f6] dark:bg-[#221c10] border-none focus:ring-2 focus:ring-primary text-sm text-[#1b170d] dark:text-white"
+              placeholder="Buscar por Raça"
+              type="text"
+              value={raca}
+              onChange={(e) => setRaca(e.target.value)}
+            />
+          </div>
+        )
+      }
     </div>
   );
 }
