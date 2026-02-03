06e55b297c5e7e05bfdbe1bfdcdbea6e4f22b063
Karine Débora Wagner <karinedwagner@gmail.com>
2026-02-02 20:53:02 -0400

    Feat: Adiciona o componente TutorCard

---
+++ src/components/TutorCard/index.tsx
@@ -0,0 +1,20 @@
+import type { ITutorContent } from '../../types/tutors';
+import { BaseCard } from '../BaseCard';
+
+interface TutorCardProps {
+    tutor: ITutorContent;
+}
+
+export function TutorCard({ tutor }: TutorCardProps) {
+    return (
+        <BaseCard
+            to={`/tutors/${tutor.id}`}
+            imageUrl={tutor.foto?.url}
+            imageAlt={tutor.foto?.nome}
+            title={tutor.nome}
+            badge="Tutor"
+            subtitle={tutor.email}
+            description={tutor.telefone}
+        />
+    );
+}
