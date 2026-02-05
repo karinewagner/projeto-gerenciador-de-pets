4848e647d7e5afdcb023841fe16ccfdefc0d3143
Karine Débora Wagner <karinedwagner@gmail.com>
2026-02-05 16:49:30 -0400

    Feat: Adicionar lazy loading routes nos módulos de pets e tutors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { MainLayout } from './components/MainLayout';
 import { ProtectedRoute } from './components/ProtectedRoute';
@@ -7,42 +8,44 @@ import { ToastProvider } from './contexts/ToastContext';
 import { ConfirmProvider } from './contexts/ConfirmContext';
 
 import { LoginPage } from './pages/LoginPage';
-import { DashboardPetsPage } from './pages/DashboardPetsPage';
-import { DashboardTutorsPage } from './pages/DashboardTutorsPage';
-import { PetDetailPage } from './pages/PetDetailPage';
-import { PetFormPage } from './pages/PetFormPage';
-import { TutorDetailPage } from './pages/TutorDetailPage';
-import { TutorFormPage } from './pages/TutorFormPage';
 import { LinkPetTutorPage } from './pages/LinkPetTutorPage';
 import { ContactsPage } from './pages/ContactsPage';
+
+const PetsRoutes = lazy(() =>
+  import('./modules/pets/PetsRoutes').then(module => ({
+    default: module.PetsRoutes,
+  }))
+);
+
+const TutorsRoutes = lazy(() =>
+  import('./modules/tutors/TutorsRoutes').then(module => ({
+    default: module.TutorsRoutes,
+  }))
+);
 
 function App() {
   return (
     <ToastProvider>
       <ConfirmProvider>
-        <Routes>
-          {/* Rotas públicas */}
-          <Route element={<MainLayout />}>
-            <Route path="/login" element={<LoginPage />} />
-          </Route>
-
-          {/* Rotas privadas */}
-          <Route element={<ProtectedRoute />}>
+        <Suspense fallback={<div className="p-6">Carregando...</div>}>
+          <Routes>
+            {/* Rotas públicas */}
             <Route element={<MainLayout />}>
-              <Route path="/pets" element={<DashboardPetsPage />} />
-              <Route path="/pets/:petId" element={<PetDetailPage />} />
-              <Route path="/pets/edit/:id" element={<PetFormPage />} />
-              <Route path="/pets/new" element={<PetFormPage />} />
-              <Route path="/tutors" element={<DashboardTutorsPage />} />
-              <Route path="/tutors/:tutorId" element={<TutorDetailPage />} />
-              <Route path="/tutors/edit/:id" element={<TutorFormPage />} />
-              <Route path="/tutors/new" element={<TutorFormPage />} />
-              <Route path="/linking" element={<LinkPetTutorPage />} />
-              <Route path="/contacts" element={<ContactsPage />} />
-
+              <Route path="/" element={<Navigate to="/login" replace />} />
+              <Route path="/login" element={<LoginPage />} />
             </Route>
-          </Route>
-        </Routes>
+
+            {/* Rotas privadas */}
+            <Route element={<ProtectedRoute />}>
+              <Route element={<MainLayout />}>
+                <Route path="/pets/*" element={<PetsRoutes />} />
+                <Route path="/tutors/*" element={<TutorsRoutes />} />
+                <Route path="/linking" element={<LinkPetTutorPage />} />
+                <Route path="/contacts" element={<ContactsPage />} />
+              </Route>
+            </Route>
+          </Routes>
+        </Suspense>
       </ConfirmProvider>
     </ToastProvider>
   );
