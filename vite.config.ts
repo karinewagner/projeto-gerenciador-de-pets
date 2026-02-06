16c2d6222775549035c4a8f0bfb056c73782e21a
Karine Débora Wagner <karinedwagner@gmail.com>
2026-02-05 23:11:35 -0400

    Corrige o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/projeto-gerenciador-de-pets/' : '/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/test/setup.ts',
   },
-});
+}));
